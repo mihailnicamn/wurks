d08ddb524a74662ca9bd2a8aab97913de645aaea
Mihail Nica <110503052+mihailnicamn@users.noreply.github.com>
2023-12-24 21:33:48 +0200

    Create clickable.js

---
+++ clickable.js
@@ -0,0 +1,17 @@
+const selector = '.clickable';
+const wait = (ms) => new Promise((r) => setTimeout(r, ms));
+const getTimeout = () => Math.random() * 100
+export default async (page) => {
+    console.log('started clickable job')
+    let iterator = 0;
+    while (iterator < 100) {
+        console.log(`clickable job #${iterator}`);
+      await wait(getTimeout());
+      const clickable = await page.$(selector);
+      if (clickable) {
+          console.log('clicked');
+        await page.click(selector);
+      }
+      iterator++;
+    }
+}
